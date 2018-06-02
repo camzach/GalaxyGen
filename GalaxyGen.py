303ca8263e75c8192ccd30e74d8e48e3cb48e070
Cameron Zach <cameron.zach@temple.edu>
2018-06-02 01:50:37 -0400

    Add generated galaxy to control page

--- GalaxyGen.py
+++ GalaxyGen.py
@@ -9,23 +9,46 @@ map = [(3, 4), (4, 5), (4, 7), (3, 8), (2, 7), (2, 5),
        (4, 1), (5, 2), (6, 5), (6, 7), (5, 10), (4, 11), (2, 11), (1, 10), (0, 7), (0, 5), (1, 2), (2, 1)]
 
 h, w = 490, 530
-x_adj = w * 3 / 4
-y_adj = h / 2
 
 def setpath(rootpath):
     global tilepath
     tilepath = rootpath + '/Tiles/'
 
-def addMecatol(galaxy):
+def addMecatol(galaxy, size):
+    tilewidth = w
+    tileheight = h
+    if size == 0:
+        tilewidth = int(w * 1/4)
+        tileheight = int(h * 1/4)
+    if size == 1:
+        tilewidth = int(w * 1/2)
+        tileheight = int(h * 1/2)
+        
+    x_adj = tilewidth * 3 / 4
+    y_adj = tileheight / 2
+    
     system = '18.png'
     tile = Image.open(tilepath + system)
+    tile = tile.resize((tilewidth, tileheight), Image.ANTIALIAS)
     x = int(x_adj * 3)
     y = int(y_adj * 6)
     galaxy.paste(tile, (x, y), tile)
 
 
-def genGalaxy(systems):
-    result = Image.new("RGBA", (w * 7, h * 7))
+def genGalaxy(systems, size):
+    tilewidth = w
+    tileheight = h
+    if size == 0:
+        tilewidth = int(w * 1/4)
+        tileheight = int(h * 1/4)
+    if size == 1:
+        tilewidth = int(w * 1/2)
+        tileheight = int(h * 1/2)
+      
+    x_adj = tilewidth * 3 / 4
+    y_adj = tileheight / 2
+      
+    result = Image.new("RGBA", (tilewidth * 7, tileheight * 7))
 
     systems = systems[:30]
 
@@ -34,11 +57,12 @@ def genGalaxy(systems):
             continue
         system = system + '.png'
         tile = Image.open(tilepath + system)
+        tile = tile.resize((tilewidth, tileheight), Image.ANTIALIAS)
         x = int(x_adj * map[index][0])
         y = int(y_adj * map[index][1])
         result.paste(tile, (x, y), tile)
 
-    addMecatol(result)
+    addMecatol(result, size)
 
     bbox = result.getbbox()
     result = result.crop(bbox)
--- flaskapp.py
+++ flaskapp.py
@@ -2,7 +2,7 @@ import GalaxyGen
 import os
 from io import BytesIO
 from random import sample
-from flask import Flask, send_file, current_app
+from flask import Flask, send_file, current_app, request
 app = Flask(__name__)
 
 GalaxyGen.setpath(app.root_path)
@@ -21,8 +21,9 @@ def help():
 
 @app.route('/galaxy/generate/<tiles>')
 def generate(tiles):
+  size = request.args.get('size', default = 1, type = int)
   systems = tiles.split(',')
-  galaxy = GalaxyGen.genGalaxy(systems)
+  galaxy = GalaxyGen.genGalaxy(systems, size)
   byte_io = BytesIO()
   galaxy.save(byte_io, 'PNG')
   byte_io.seek(0)
@@ -30,8 +31,9 @@ def generate(tiles):
 
 @app.route('/galaxy/fullrandom')
 def fullrandom():
+  size = request.args.get('size', default = 1, type = int)
   systems = list(map(lambda x: str(x), sample(range(19, 51), 30)))
-  galaxy = GalaxyGen.genGalaxy(systems)
+  galaxy = GalaxyGen.genGalaxy(systems, size)
   byte_io = BytesIO()
   galaxy.save(byte_io, 'PNG')
   byte_io.seek(0)
